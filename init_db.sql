8cfd4b04c3805405fa74035baf8def930c4fd7b3
jpabloAg <39423038+jpabloAg@users.noreply.github.com>
2022-09-12 00:43:21 -0500

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -4,3 +4,8 @@ CREATE TABLE accounts (
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
+
+INSERT INTO accounts (user_id, username, password, email) VALUES('1','test1','aww','test1@gmail.com');
+INSERT INTO accounts (user_id, username, password, email) VALUES('2','test2','x9p','test2@gmail.com');
+
+SELECT	* FROM accounts;
